446cab1b2fcc2c804989bcfe9e87fcf8eac85cbb
Maria Maas <meagmaas@student.che.nl>
2024-01-30 14:15:07 +0100

    Add project files.

---
+++ Count von Cal/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Count_von_Cal.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
